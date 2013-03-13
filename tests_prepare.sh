a5aab683b599c1e4606f354497699cfddc4f2819
jbbarth <jeanbaptiste.barth@gmail.com>
2013-03-13 10:34:22 +0100

    Copy fixtures in tests_prepare.sh

--- tests_prepare.sh
+++ tests_prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export RAILS_ENV=test
+cp -f plugins/*/test/fixtures/*yml test/fixtures/
 rake db:drop db:create db:migrate redmine:plugins db:schema:dump db:fixtures:load
-cp db/test.sqlite3 db/test.template.sqlite3
+cp -f db/test.sqlite3 db/test.template.sqlite3
