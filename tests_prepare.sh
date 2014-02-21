1a513e3df054378a6917aadcd73967eb3412da3c
jbbarth <jeanbaptiste.barth@gmail.com>
2014-02-21 09:03:26 +0100

    Backup original core test/fixtures directory then replace it with its original copy each time the test suite is run

--- tests_prepare.sh
+++ tests_prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export RAILS_ENV=test
+test -e test/.fixtures.core || cp -a test/fixtures test/.fixtures.core
+rm -rf test/fixtures
+cp -a test/.fixtures.core test/fixtures
 cp -i plugins/*/test/fixtures/*yml test/fixtures/
 bundle exec rake db:drop db:create db:migrate redmine:plugins db:schema:dump db:fixtures:load
 cp -f db/test.sqlite3 db/test.template.sqlite3
