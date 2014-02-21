6bfb0e653f25a11416eb0fadc32bfae514d25947
jbbarth <jeanbaptiste.barth@gmail.com>
2014-02-21 09:01:13 +0100

    Don't replace core fixtures silently, always prompt what to do

--- tests_prepare.sh
+++ tests_prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RAILS_ENV=test
-cp -f plugins/*/test/fixtures/*yml test/fixtures/
+cp -i plugins/*/test/fixtures/*yml test/fixtures/
 bundle exec rake db:drop db:create db:migrate redmine:plugins db:schema:dump db:fixtures:load
 cp -f db/test.sqlite3 db/test.template.sqlite3
