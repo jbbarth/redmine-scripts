e45834ef937863e8f93acd50dfc868d734c4b161
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-22 19:36:48 +0200

    Also copy fixtures inside <plugin>/spec/fixtures directories in test_prepare.sh

--- tests_prepare.sh
+++ tests_prepare.sh
@@ -4,5 +4,6 @@ test -e test/.fixtures.core || cp -a test/fixtures test/.fixtures.core
 rm -rf test/fixtures
 cp -a test/.fixtures.core test/fixtures
 cp -i plugins/*/test/fixtures/*yml test/fixtures/
+cp -i plugins/*/spec/fixtures/*yml test/fixtures/
 bundle exec rake db:drop db:create db:migrate redmine:plugins db:schema:dump #db:fixtures:load
 cp -f db/test.sqlite3 db/test.template.sqlite3
